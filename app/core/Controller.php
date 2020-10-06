d13431eaa818bf4489ccde1c1196cb5872cd0d8c
jonathanmarp <60574415+jonathanmarp@users.noreply.github.com>
2020-10-06 15:23:07 +0700

    Update Controller
    Update For Binary Word

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -32,4 +32,27 @@ class Controller {
 
         return $decryption;
     }
+    public function strigToBinary($string)
+    {
+        $characters = str_split($string);
+     
+        $binary = [];
+        foreach ($characters as $character) {
+            $data = unpack('H*', $character);
+            $binary[] = base_convert($data[1], 16, 2);
+        }
+     
+        return implode(' ', $binary);    
+    }
+    public function binaryToString($binary)
+    {
+        $binaries = explode(' ', $binary);
+     
+        $string = null;
+        foreach ($binaries as $binary) {
+            $string .= pack('H*', dechex(bindec($binary)));
+        }
+     
+        return $string;    
+    }
 }
