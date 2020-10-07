31210b5ef889eb839c2e48c213797d3b5bd2df18
thekotekjournal <63573571+thekotekjournal@users.noreply.github.com>
2020-10-07 12:28:42 +0700

    Ok
    Nice

--- app/views/Home/index.php
+++ app/views/Home/index.php
@@ -23,7 +23,7 @@
       <div class="row">
           <div class="col-sm">
             <div class="container">
-              <img src="<?= base_url; ?>assets/img/Rocket.gif" alt="rocket-live">
+              <img src="<?= base_url; ?>assets/img/Rocket.gif" alt="rocket-live-system">
             </div>
           </div>
           <div class="col-sm">
