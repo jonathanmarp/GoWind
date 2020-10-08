19090c35fcee2fd87e088334fb207c293f6ad7ae
thekotekjournal <63573571+thekotekjournal@users.noreply.github.com>
2020-10-08 10:46:09 +0700

    Fixed
    Fixed

--- app/init.php
+++ app/init.php
@@ -4,6 +4,6 @@ require_once 'core/App.php';
 require_once 'core/Controller.php';
 require_once 'core/Database.php';
 require_once 'core/VanDB.php';
-require_once 'core/Api.php';
+// require_once 'core/Api.php';
 
 require_once 'config/config.php';
